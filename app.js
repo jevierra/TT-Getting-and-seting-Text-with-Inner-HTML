761a595fb0d842289e270a6d26c9dd3176e078af
jeffrey vierra <st.pious@gmail.com>
2017-08-15 07:50:20 -1000

    Fixed JS issues
    Changed the const descriptions
    Added a create button and element

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const input = document.querySelector('input');
-const p = document.querySelector('p.description');
-const button = document.querySelector('button');
+
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+
+	/* Added new a description prefix to three new constants */
+const descriptionInput = document.querySelector('input.description');
+const descriptionP = document.querySelector('p.description');
+const descriptionButton = document.querySelector('button.description');
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('button.addItemButton');
+
+document.textContent
 
 /*
 
@@ -12,11 +21,43 @@ p.textContent = input.value + ':'
 
 })
 */
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display == 'none') {
+    toggleList.textContent = 'Hide list';
+    listDiv.style.display = 'block';
+  } else {
+    toggleList.textContent = 'Show list';                        
+    listDiv.style.display = 'none';
+  }                         
+});
+
+descriptionButton.addEventListener('click', () => {
+  descriptionP.innerHTML = descriptionInput.value + ':';
+});
+
+addItemButton.addEventListener('click', () => {
+  let li = document.createElement('li');
+  li.textContent = addItemInput.value;
+});
+  
+  
+  
+  
+  
 
 
-button.addEventListener('click', () => {
+ addItemButton.addEventListener('click', () => {
 
-p.innerHTML = input.value + ':'
+ 	let li = document.createElement('li');
+ 	li.textContent = addItemInput.value;
 
 
-})
+ });
+
+
+
+
+
+
+
+/*p.title = "list description";*/
